c32934b809bd731f5261cdd5113b2b6cc3977e90
3bd alra7man nassef <131453684+3bdUra7manNassef@users.noreply.github.com>
2023-04-25 17:49:42 +0300

    Add files via upload

---
+++ name_and_status.py
@@ -0,0 +1,26 @@
+#name and status# 
+
+
+
+#*we need inputs and var to print
+
+#*if 
+
+#todo: (*1 var in inputs to enter name*) 
+people_1 = input('enter first name: ') #! names the people 
+people_2 = input('enter second name: ')
+people_3 = input('enter third name: ')
+
+#* ((we want a status people)) *
+
+status_people_1 = input('\nenter a status ' + people_1 + ': ')#! status the people
+status_people_2 = input('enter a status ' + people_2 + ': ')
+status_people_3 = input('enter a status ' + people_3 + ': ')
+
+#*ok we want to printer names and status
+
+#todo: printer a names and people
+
+print(people_1+':'+status_people_1) #! first people and status
+print(people_2+':'+status_people_2) #! second people and status
+print(people_3+':'+status_people_3) #! third people and status
